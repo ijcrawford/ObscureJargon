9bb222a44761962936f376545cea6d27e7e86d52
ijcrawford <ijcrawford@quinnipiac.edu>
2021-02-23 17:33:10 -0500

    Polished for final build

--- src/warpSkip.java
+++ src/warpSkip.java
@@ -1,10 +1,10 @@
 public class warpSkip {
 
     public warpSkip() {
-        System.out.println("Warp-skipping is a strategy often implemented by speedrunners or skilled" +
-                " players who simply want to complete the chapter faster. The process involves" +
-                " making generous use of the warp staff/spell to transport one or multiple units" +
-                " across the map in a manner that allows the player to skip parts of or the" +
-                " entirety of a map.");
+        System.out.println("Warp-skipping is a strategy often implemented by speedrunners or skilled\n" +
+                "players who simply want to complete the chapter faster. The process involves\n" +
+                "making generous use of the warp staff/spell to transport one or multiple units\n" +
+                "across the map in a manner that allows the player to skip parts of or the\n" +
+                "entirety of a map.");
     }
 }
