5755850b8f977780395327961430f4762496ae1e
ijcrawford <ijcrawford@quinnipiac.edu>
2021-02-23 16:56:14 -0500

    Added enemy control glitch

---
+++ src/EnemyControlGlitch.java
@@ -0,0 +1,9 @@
+public class EnemyControlGlitch {
+
+    public EnemyControlGlitch() {
+        System.out.println("The Enemy Control Glitch is, as the name suggests, a glitch performable in" +
+                " both The Blazing Blade and The Sacred Stones which when performed correctly allows" +
+                " the player to take control of enemy units during their phase. It is also  known" +
+                " as the Mine Glitch, since the glitch can be performed using the Mine item.");
+    }
+}
